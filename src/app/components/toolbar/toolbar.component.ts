5d49d2e0d2aca1632bc1d8c79fe84bbdd08d0d63
Ernesto Herrera <jesh40@hotmail.es>
2022-09-24 19:55:04 -0600

    implement dialog

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { PeopleTableComponent } from '../people-table';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule],
+  imports: [MatToolbarModule, MatIconModule, MatDialogModule],
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
+  openDialog(
+    enterAnimationDuration: string,
+    exitAnimationDuration: string
+  ): void {
+    this.dialog.open(PeopleTableComponent, {
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
 
   ngOnInit(): void {}
 }
